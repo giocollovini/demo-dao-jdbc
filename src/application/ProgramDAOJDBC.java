8e6731f1a7571f3ae06a974dc1b2cb48b463d5cf
Giovani Collovini <gcollovini@gmail.com>
2020-04-22 13:45:31 -0300

    Criação findById Implementation

--- src/application/ProgramDAOJDBC.java
+++ src/application/ProgramDAOJDBC.java
@@ -1,23 +1,22 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class ProgramDAOJDBC {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		//Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		//System.out.println(obj);
 		
-		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		//Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); 
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
