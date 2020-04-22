cf46708f189a70a38a7c141f740a40b2c0144557
Giovani Collovini <gcollovini@gmail.com>
2020-04-22 14:02:05 -0300

    Reusando Instanciação

--- src/application/ProgramDAOJDBC.java
+++ src/application/ProgramDAOJDBC.java
@@ -13,6 +13,7 @@ public class ProgramDAOJDBC {
 		//System.out.println(obj);
 		
 		//Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println("=== TEST 1: seller findById ===");
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); 
 		
